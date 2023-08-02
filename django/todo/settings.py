66c846400c0b966e589d8d7270a8c624e9a8675a
Lazokat <137065848+Lazokat@users.noreply.github.com>
2023-08-02 07:47:08 +0500

    Update settings.py

--- django/todo/settings.py
+++ django/todo/settings.py
@@ -88,7 +88,7 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default=postgres://todo_mog4_user:vDF3Zm9JhS1kjVFyvBnWc2DMBwcjKG31@dpg-cj4s5khitvpc73eqgnpg-a.oregon-postgres.render.com/todo_mog4,
+        default='postgres://todo_mog4_user:vDF3Zm9JhS1kjVFyvBnWc2DMBwcjKG31@dpg-cj4s5khitvpc73eqgnpg-a.oregon-postgres.render.com/todo_mog4',
         conn_max_age=600)
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_tables',                      
