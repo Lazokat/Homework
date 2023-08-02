8a50ed4952ca876c8b9b3db699c27ff34aa084bd
Lazokat <137065848+Lazokat@users.noreply.github.com>
2023-08-02 06:44:39 +0500

    Update settings.py

--- django/todo/settings.py
+++ django/todo/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
