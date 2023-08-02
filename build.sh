fd8e26c230affafd1b7eae9a0ef9d5893b9c8a05
Lazokat <137065848+Lazokat@users.noreply.github.com>
2023-08-02 06:57:54 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+set -o errexit  # exit on error
 
-poetry install
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-gunicorn todo.wsgi:application
