f468bf13d94d2b49e2a36be2869eebb668d3698d
Lazokat <137065848+Lazokat@users.noreply.github.com>
2023-08-02 06:45:07 +0500

    Update settings.py

--- django/todo/settings.py
+++ django/todo/settings.py
@@ -18,10 +18,8 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
